ed73cfd081e4c03f730411e5797009ca4cf6186d
IT24101545 <it24101545@my.sllit.lk>
2026-09-14 10:36:55 +0530

    updated routes

--- web/smart-property-web/src/routes/AppRoutes.jsx
+++ web/smart-property-web/src/routes/AppRoutes.jsx
@@ -1,8 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import TenantListPage from "../features/tenancies/pages/TenantListPage";
 import AddTenantPage from "../features/tenancies/pages/AddTenantPage";
 import TenantDetailsPage from "../features/tenancies/pages/TenantDetailsPage";
 
-// inside your <Routes> block:
-<Route path="/owner/tenants" element={<TenantListPage />} />
-<Route path="/owner/tenants/add" element={<AddTenantPage />} />
-<Route path="/owner/tenants/:id" element={<TenantDetailsPage />} />
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/owner/tenants" element={<TenantListPage />} />
+      <Route path="/owner/tenants/add" element={<AddTenantPage />} />
+      <Route path="/owner/tenants/:id" element={<TenantDetailsPage />} />
+    </Routes>
+  );
+}
