e192fc1bdd6e1ae0f331af6075497da11bcbb2ab
IT24101545 <it24101545@my.sllit.lk>
2026-09-14 10:31:39 +0530

    updated program cs

--- backend/SmartProperty.Api/Program.cs
+++ backend/SmartProperty.Api/Program.cs
@@ -167,7 +167,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseCors("AllowClients");
 
