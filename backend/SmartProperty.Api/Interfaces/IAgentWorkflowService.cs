f832af7c50264caaf0efa13a3fb6278696e5a550
Owner <IT24101405@my.sliit.lk>
2026-09-24 07:16:07 +0530

    Implement Agent 1 workflow service

---
+++ backend/SmartProperty.Api/Interfaces/IAgentWorkflowService.cs
@@ -0,0 +1,12 @@
+using SmartProperty.Api.DTOs.AgenticAI;
+
+namespace SmartProperty.Api.Interfaces;
+
+public interface IAgentWorkflowService
+{
+    Task<WorkflowResponseDto> StartPlannerWorkflowAsync(int maintenanceRequestId, CancellationToken cancellationToken = default);
+
+    Task<WorkflowResponseDto?> GetWorkflowByIdAsync(int workflowId, CancellationToken cancellationToken = default);
+
+    Task<WorkflowResponseDto?> GetWorkflowByRequestIdAsync(int maintenanceRequestId, CancellationToken cancellationToken = default);
+}
